53dfc7b19f4cd91d0a3cb6401b9b17ee7ecd9f8c
Victor Williams <vrwilliams719@gmail.com>
2023-12-14 02:45:53 -0500

    version: 1.0.1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const snappleFacts_json_1 = __importDefault(require("./snappleFacts.json"));
+const snappleFacts = snappleFacts_json_1.default.snapplefacts;
+exports.default = snappleFacts;
---
+++ src/tests/index.test.js
@@ -0,0 +1,6 @@
+"use strict";
+describe('Test', () => {
+    it('should pass', () => {
+        expect(true).toBe(true);
+    });
+});
