71f500ae8165998f3e5dff24fed3d90513bc3335
Victor Williams <vrwilliams719@gmail.com>
2023-12-31 17:44:20 -0500

    Refactor randomIndex generation to use crypto.randomInt()

--- src/functions/randomFact/randomFact.ts
+++ src/functions/randomFact/randomFact.ts
@@ -1,12 +1,12 @@
 import { SnappleFact } from '../../interfaces/snappleFact';
-
+import crypto from 'crypto';
 /**
  * Retrieves a random SnappleFact from the given array of SnappleFacts.
  * @param {SnappleFact[]} snappleFacts - The array of SnappleFacts to choose from.
  * @returns {SnappleFact} - A random SnappleFact.
  */
 const RandomFact = (snappleFacts: SnappleFact[]): SnappleFact => {
-  const randomIndex = Math.floor(Math.random() * snappleFacts.length);
+  const randomIndex = crypto.randomInt(0, snappleFacts.length);
   return snappleFacts[randomIndex];
 };
 
