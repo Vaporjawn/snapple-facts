0b08f73feaa0df4c59c50b5768ce6a70f89b38bd
Victor Williams <vrwilliams719@gmail.com>
2024-01-01 12:16:00 -0500

    v2.1.3 publish

--- src/functions/getFacts/getFacts.js
+++ src/functions/getFacts/getFacts.js
@@ -1,16 +1,14 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const snappleFacts_1 = __importDefault(require('../../snappleFacts'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const snappleFacts_1 = __importDefault(require("../../snappleFacts"));
 /**
  * Retrieves an array of Snapple facts.
  * @returns {SnappleFact[]} An array of Snapple facts.
  */
 const GetFacts = () => {
-  return snappleFacts_1.default;
+    return snappleFacts_1.default;
 };
 exports.default = GetFacts;
