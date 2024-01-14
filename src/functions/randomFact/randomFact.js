6b9f51d5512af434a714b90b5669f3ac6569209c
Victor Williams <vrwilliams719@gmail.com>
2024-01-14 15:29:01 -0500

    2.2.1

--- src/functions/randomFact/randomFact.js
+++ src/functions/randomFact/randomFact.js
@@ -1,18 +1,16 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const crypto_1 = __importDefault(require('crypto'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
 /**
  * Retrieves a random SnappleFact from the given array of SnappleFacts.
  * @param {SnappleFact[]} snappleFacts - The array of SnappleFacts to choose from.
  * @returns {SnappleFact} - A random SnappleFact.
  */
-const RandomFact = snappleFacts => {
-  const randomIndex = crypto_1.default.randomInt(0, snappleFacts.length);
-  return snappleFacts[randomIndex];
+const RandomFact = (snappleFacts) => {
+    const randomIndex = crypto_1.default.randomInt(0, snappleFacts.length);
+    return snappleFacts[randomIndex];
 };
 exports.default = RandomFact;
